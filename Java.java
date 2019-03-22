62bd4ad45d4815e9fc8fb3e3652cfc72cf54eef4
Jeroen Broks <facebook@tbbs.nl>
2019-03-22 11:19:09 +0100

    Create Java.java

---
+++ Java.java
@@ -0,0 +1,8 @@
+// Please note you'll have to rename this file to HelloWorldApp.java to make it work
+// I now named it Java.java in order to keep this repository in order
+
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Prints the string to the console.
+    }
+}
